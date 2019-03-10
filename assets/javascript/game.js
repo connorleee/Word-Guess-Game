56cc7a3afe42a1ea48f89613d4087bd602dec558
Connor Lee <45024235+connorleee@users.noreply.github.com>
2019-03-10 15:35:36 -0700

    minor html changes and initial variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+// Possible words to guess
+var words = [
+    a1,
+    b12,
+    c123,
+    d1234,
+    e12345,
+    f123456,
+]
+
+// Counters
+var wins = 0;
+var losses = 0;
+var gamesPlayed = 0;
+var remainingGuesses = 10;
+
+// Blank arrays
+var word = [];
+var incorrectKeys = [];
+
+// grabbing html id's
