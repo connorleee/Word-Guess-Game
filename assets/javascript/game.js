6b29184f99e3fe26a45f6c7c1c097b52caa0899b
Connor Lee <45024235+connorleee@users.noreply.github.com>
2019-03-10 20:58:27 -0700

    created array for word to guess. this happens on key press, but needs to happen once per game.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,27 @@
 // Possible words to guess
 var words = [
-    a1,
-    b12,
-    c123,
-    d1234,
-    e12345,
-    f123456,
+    "audi",
+    "bmw",
+    "cadillac",
+    "chevrolet",
+    "chrysler",
+    "dodge",
+    "ferrari",
+    "ford",
+    "honda",
+    "jaguar",
+    "jeep",
+    "lamborghini",
+    "lexus",
+    "mazda",
+    "mercedes",
+    "nissan",
+    "porsche",
+    "subaru",
+    "tesla",
+    "toyota",
+    "volkswagon",
+    "volvo"
 ]
 
 // Counters
@@ -13,10 +29,6 @@ var wins = 0;
 var losses = 0;
 var gamesPlayed = 0;
 var remainingGuesses = 10;
-
-// Blank arrays
-var word = [];
-var incorrectKeys = [];
 
 // grabbing html id's
 var winsEl = document.getElementById("wins");
@@ -26,10 +38,28 @@ var wordEl = document.getElementById("word");
 var incorrectKeysEl = document.getElementById("incorrect");
 var remainingGuessesEl = document.getElementById("remaining-guesses");
 
-// keypress event to initialize game
+// keypress event to guess letters
 document.onkeyup = function(event) {
+    // Blank arrays
+    var word = [];
+    var incorrectKeys = [];
+    
+    // variable for unguessed letters
+    var us = " ___ ";
 
+    // Choose a random word from the words array
+    var rndWord = words[Math.floor(Math.random()*words.length)];
+    console.log(rndWord);
+    var wordLength = rndWord.length;
+    console.log(wordLength);
 
+    // Display number of underscores cooresponding with length of random word
+   for (let i = 0; i < wordLength; i++) {  
+        word[i] = us;
+    }
+    console.log(word);
+    
+   
 
 
 
@@ -39,4 +69,5 @@ document.onkeyup = function(event) {
     lossesEl.textContent = losses;
     gamesPlayedEl.textContent = gamesPlayed;
     remainingGuessesEl.textContent = remainingGuesses;
+    wordEl.textContent = word;
 }
